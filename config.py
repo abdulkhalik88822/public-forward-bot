4d7a57b76a783ec571e4c1f9dc265f5b6b3dfa9e
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-03-12 20:39:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://kailash:pass@cluster0.sqtztxm.mongodb.net/?retryWrites=true&w=majority")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
-    SESSION = os.environ.get("SESSION", "BQBf4zKtsfs99UCht1XMEcIFZEkGGDfhiHuMmAa8oOa59RnAEzRfrrj9zV3s0qCssTzgozmbJFnhVLpymamy6-JLCIV0SrQT9qdA8ZjIAYjWIn_lN6M7yAysItJKcQfMUraAHWG03o4pRNSBD4zeMyrPO16ktDMzlB0mLIUGQ7S3v5WLzVVlW8z9Rm3DWlNJ8y9fKJBpuZ6WxDdcaPvc33cWlZk0MoQ5quJH0lNQemk2K2VohBzSQSk9hNVdSX0HaZoLbJe30vZyKanDFqT6oCWMbItgbenvB-e1oTpTi6OzKHMK10JRa_Ib9lwTvOP_xUzL3SmRfZ2yiEF_gocl39ksAAAAAWyu0rsA")   
+    SESSION = os.environ.get("SESSION", "BQCb5jnX1x69kTAqq2uOD1vUGHxrmDc7M2TvlX5W47iWpbafIgPMLoP-LdPwPuVZDo74x-Lf3nFnr03xj4NlHIeSvbjqXZXDdVhnebAp5Y8AD7Msn2z1j3f9Fkjs8hn14_EBFyPYACly8O9_NfbBDtkZtIur7uwkeR76_JuL2KqopoPHt3R59JbtacC8uMT2LqvtyvKtBvQm7bB3Z7RgMmsvKaYmF2QmUMTr0zZmNlAogFMDMD5o7V8KawSrS4eImbu9jF6CiQ8RAPyYKUxPyx6zBMc_0w1ygOB-Mf45JqxQZSlwPcyLYi6M1xmhUilHIHa0SNOMckGzcVeHv7RNLU-rAAAAAWyu0rsA")   
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001626107740"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "Auto_forword_dd_bot")
 
