acf6d270965cdc02a804b4dabc822329b3563e57
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-03-12 20:57:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
     SESSION = os.environ.get("SESSION", "BQCb5jnX1x69kTAqq2uOD1vUGHxrmDc7M2TvlX5W47iWpbafIgPMLoP-LdPwPuVZDo74x-Lf3nFnr03xj4NlHIeSvbjqXZXDdVhnebAp5Y8AD7Msn2z1j3f9Fkjs8hn14_EBFyPYACly8O9_NfbBDtkZtIur7uwkeR76_JuL2KqopoPHt3R59JbtacC8uMT2LqvtyvKtBvQm7bB3Z7RgMmsvKaYmF2QmUMTr0zZmNlAogFMDMD5o7V8KawSrS4eImbu9jF6CiQ8RAPyYKUxPyx6zBMc_0w1ygOB-Mf45JqxQZSlwPcyLYi6M1xmhUilHIHa0SNOMckGzcVeHv7RNLU-rAAAAAWyu0rsA")   
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001626107740"))
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001900673649"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "Auto_forword_dd_bot")
 
 
