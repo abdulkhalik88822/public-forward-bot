3563e78dbe0587f15c390e23da720d27729e43a5
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-03-12 20:34:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6265513460:AAEVABfwA8nb-OamLRm_aTEzP9kUlsnvzos")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
     OWNER_ID = os.environ.get("OWNER_ID", "5941212132")                             
-    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Abdul12:Abdul12@cluster0.o6jx2be.mongodb.net/?retryWrites=true&w=majority")  
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://kailash:pass@cluster0.sqtztxm.mongodb.net/?retryWrites=true&w=majority")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
     SESSION = os.environ.get("SESSION", "BQBf4zKtsfs99UCht1XMEcIFZEkGGDfhiHuMmAa8oOa59RnAEzRfrrj9zV3s0qCssTzgozmbJFnhVLpymamy6-JLCIV0SrQT9qdA8ZjIAYjWIn_lN6M7yAysItJKcQfMUraAHWG03o4pRNSBD4zeMyrPO16ktDMzlB0mLIUGQ7S3v5WLzVVlW8z9Rm3DWlNJ8y9fKJBpuZ6WxDdcaPvc33cWlZk0MoQ5quJH0lNQemk2K2VohBzSQSk9hNVdSX0HaZoLbJe30vZyKanDFqT6oCWMbItgbenvB-e1oTpTi6OzKHMK10JRa_Ib9lwTvOP_xUzL3SmRfZ2yiEF_gocl39ksAAAAAWyu0rsA")   
