c3888e5c78050806f2b32fd8f104654887e13cdd
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-03-15 15:19:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 import logging
 class Config:                                                                   
-    API_ID = int(os.environ.get("API_ID", "23785747"))
-    API_HASH = os.environ.get("API_HASH", "c77dded6cdf46494e51816511b9b1ece")       
+    API_ID = int(os.environ.get("API_ID", "26618097"))
+    API_HASH = os.environ.get("API_HASH", "1154f54e18b67c1239f9674c643b7bcb")       
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6265513460:AAEVABfwA8nb-OamLRm_aTEzP9kUlsnvzos")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
     OWNER_ID = os.environ.get("OWNER_ID", "5941212132")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Abdul12:Abdul12@cluster0.o6jx2be.mongodb.net/?retryWrites=true&w=majority")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
-    SESSION = os.environ.get("SESSION", "BQCb5jnX1x69kTAqq2uOD1vUGHxrmDc7M2TvlX5W47iWpbafIgPMLoP-LdPwPuVZDo74x-Lf3nFnr03xj4NlHIeSvbjqXZXDdVhnebAp5Y8AD7Msn2z1j3f9Fkjs8hn14_EBFyPYACly8O9_NfbBDtkZtIur7uwkeR76_JuL2KqopoPHt3R59JbtacC8uMT2LqvtyvKtBvQm7bB3Z7RgMmsvKaYmF2QmUMTr0zZmNlAogFMDMD5o7V8KawSrS4eImbu9jF6CiQ8RAPyYKUxPyx6zBMc_0w1ygOB-Mf45JqxQZSlwPcyLYi6M1xmhUilHIHa0SNOMckGzcVeHv7RNLU-rAAAAAWyu0rsA")   
+    SESSION = os.environ.get("SESSION", "BQCcVKrUXOUoFwF-0ZiBRPNlslEFFDPkZ8COLTJ9fe2ffvahC46DXRNdqS9ghgh1uFJxtN1j3fLgkYeXvwZ-htr233DfgDrA7y94eoyeQu321PgQ87xoGSH-Jd3TWYjox11hDgYQ5vneVZYEW4s3VJeJulWNFtfhU-whP5u1hRtE_o-f8jhLwkVef8hYdC3P0tleyZuIfmQroAuz2hdgjdsB6AlzG5zP6va3f6dDo2D3LZkljgvlIcqnzyXwm89JuB-wcBwTusX_OzPI5bkU8bNYQ52taidaptfYFFM4b1kMhTP1iru0q6arbwY1znGT8phxdc15bq0axxlsPLJi0koKAAAAATK48jYA")   
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001887724395"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "Auto_forword_dd_bot")
 
