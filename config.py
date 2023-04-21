d903bed16e6220d547ea744fc297238fc0c65f53
abdulkhalik88822 <125821386+abdulkhalik88822@users.noreply.github.com>
2023-04-21 15:43:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import logging
 class Config:                                                                   
     API_ID = int(os.environ.get("API_ID", "26618097"))
     API_HASH = os.environ.get("API_HASH", "1154f54e18b67c1239f9674c643b7bcb")       
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6265513460:AAEVABfwA8nb-OamLRm_aTEzP9kUlsnvzos")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5967250731:AAHdc_fnsJlewcP9UdKOfGPG939oVOhMY60")
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
     OWNER_ID = os.environ.get("OWNER_ID", "5941212132")                             
     DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://Abdul12:Abdul12@cluster0.o6jx2be.mongodb.net/?retryWrites=true&w=majority")  
